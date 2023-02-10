75a2dc1e891afb12a94525cfc8510e34f95ef57d
abhishekpyasi <pyasi.abhishek@gmail.com>
2023-02-10 14:28:14 +0530

    commiting after swagger API issue fixed

--- Startup.cs
+++ Startup.cs
@@ -32,11 +32,12 @@ namespace WebAPIVersioning
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIVersioning", Version = "v1" });
+                c.ResolveConflictingActions(apiDescriptions => apiDescriptions.First());
             });
 
             services.AddApiVersioning(options =>
             {
-                options.DefaultApiVersion = new ApiVersion(1,0); // used to set default version of API
+                options.DefaultApiVersion = new ApiVersion(1, 0); // used to set default version of API
                 options.AssumeDefaultVersionWhenUnspecified = true; //flag AssumeDefaultVersionWhenUnspecified flag is used to set the default version when the client has not specified any versions. 
                 options.ReportApiVersions = true; //To return the API version in response header
             });
@@ -49,6 +50,7 @@ namespace WebAPIVersioning
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
+
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPIVersioning v1"));
             }
 
